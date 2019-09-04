1122c3f1635ba11cfea2c0e80ba7eecafef31f21
Kene Ikeanyi <keneikeanyi@gmail.com>
2019-09-04 23:23:59 +0100

    first commit

---
+++ content/blog/startng/index.md
@@ -0,0 +1,6 @@
+---
+title: StartNg and Flutterwave
+date: "2019-09-03T22:12:03.284Z"
+---
+
+This is my first post on my new fake blog! How exciting!
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -31,8 +31,7 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.
+              Written by <strong>{author}</strong> who lives in Lagos learning everyday.
               {` `}
               <a href={`https://twitter.com/${social.twitter}`}>
                 You should follow him on Twitter
@@ -47,7 +46,7 @@ function Bio() {
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile-pic.jpeg/" }) {
       childImageSharp {
         fixed(width: 50, height: 50) {
           ...GatsbyImageSharpFixed
